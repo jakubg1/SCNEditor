5307f343e7ecda4448931c12a8ab98cd883bf911
jakubg1 <MASLAKMAX@Wp.pl>
2018-01-29 12:00:49 +0100

    Fixed drawing triangles
    However, they are appearing black.

--- Triangle.gd
+++ Triangle.gd
@@ -1,8 +1,9 @@
 extends ImmediateGeometry
 
 func _ready():
-	begin(0)
+	begin(Mesh.PRIMITIVE_TRIANGLES)
+	set_color(Color(0, 255, 0))
 	add_vertex(Vector3(0, 0, 0))
-	add_vertex(Vector3(0, 0, 5))
-	add_vertex(Vector3(5, 0, 5))
+	add_vertex(Vector3(3, 0, 3))
+	add_vertex(Vector3(0, 0, 3))
 	end()
